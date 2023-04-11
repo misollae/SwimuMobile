00133858610f475cf870641936f57a1a7bfe2bc3
Daniela Levezinho <daniela.levezinho@sky.uk>
2023-04-11 16:09:24 +0100

    add navigation to BluetoothScreen

--- App.tsx
+++ App.tsx
@@ -1,106 +1,22 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {
-  Text,
-  View,
-  StatusBar,
-  ScrollView,
-  StyleSheet,
-  Dimensions,
-  SafeAreaView,
-  useColorScheme,
-  TouchableOpacity,
-} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import useBLE from './useBLE';
-import { Device } from 'react-native-ble-plx';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BluetoothScreen from './HomeScreen';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const {requestPermissions, scanForDevices, allDevices} = useBLE();
-  const [isScanning, setIsScanning] = useState(false);
-
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const startScan = async () => {
-    requestPermissions((isGranted: boolean) => {
-        if (isGranted) {
-          setIsScanning(true);
-          scanForDevices();
-        }
-    });
-  };
-
   return (
-    <SafeAreaView style={[backgroundStyle, styles.mainBody]}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}
-        contentContainerStyle={styles.mainBody}
-        contentInsetAdjustmentBehavior="automatic">
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-            marginBottom: 40,
-          }}>
-          <View>
-            <Text
-              style={{
-                fontSize: 30,
-                textAlign: 'center',
-                color: isDarkMode ? Colors.white : Colors.black,
-              }}>
-              Bluetooth Test
-            </Text>
-          </View>
-          <TouchableOpacity
-            activeOpacity={0.5}
-            style={styles.buttonStyle}
-            onPress={startScan}>
-            <Text style={styles.buttonTextStyle}>
-              {isScanning ? 'Scanning...' : 'Scan Bluetooth Devices'}
-            </Text>
-          </TouchableOpacity>
-          {
-            allDevices.map((device: Device) => (
-              <Text>{device.name}</Text>
-            ))
-          }
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="BluetoothScreen"
+          component={BluetoothScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-const windowHeight = Dimensions.get('window').height;
-const styles = StyleSheet.create({
-  mainBody: {
-    flex: 1,
-    justifyContent: 'center',
-    height: windowHeight,
-  },
-  buttonStyle: {
-    backgroundColor: '#307ecc',
-    borderWidth: 0,
-    color: '#FFFFFF',
-    borderColor: '#307ecc',
-    height: 40,
-    alignItems: 'center',
-    borderRadius: 30,
-    marginLeft: 35,
-    marginRight: 35,
-    marginTop: 15,
-  },
-  buttonTextStyle: {
-    color: '#FFFFFF',
-    paddingVertical: 10,
-    fontSize: 16,
-  },
-});
 
 export default App;
--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,16 +1,108 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {Button} from 'react-native/types';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
+import {useState} from 'react';
+import {Device} from 'react-native-ble-plx';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  useColorScheme,
+  StatusBar,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+import useBLE from './useBLE';
 
-const HomeScreen = ({navigation}) => {
+const BluetoothScreen = () => {
+  const {requestPermissions, scanForDevices, allDevices} = useBLE();
+  const [isScanning, setIsScanning] = useState(false);
+
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const startScan = async () => {
+    requestPermissions((isGranted: boolean) => {
+      if (isGranted) {
+        setIsScanning(true);
+        scanForDevices();
+      }
+    });
+  };
   return (
-    <NavigationContainer>
+    <SafeAreaView style={[backgroundStyle, styles.mainBody]}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        style={backgroundStyle}
+        contentContainerStyle={styles.mainBody}
+        contentInsetAdjustmentBehavior="automatic">
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+            marginBottom: 40,
+          }}>
+          <View>
+            <Text
+              style={{
+                fontSize: 30,
+                textAlign: 'center',
+                color: isDarkMode ? Colors.white : Colors.black,
+              }}>
+              Bluetooth Test
+            </Text>
+          </View>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            style={styles.buttonStyle}
+            onPress={startScan}>
+            <Text style={styles.buttonTextStyle}>
+              {isScanning ? 'Scanning...' : 'Scan Bluetooth Devices'}
+            </Text>
+          </TouchableOpacity>
+          {allDevices.map((device: Device) => (
+            <Text>{device.name}</Text>
+          ))}
+        </View>
+      </ScrollView>
       <Button
         title="Go to HomeScreen"
-        onPress={() => navigation.navigate('Home')}
+        // onPress={() => navigation.navigate('Home')}
       />
-    </NavigationContainer>
+    </SafeAreaView>
   );
 };
+const windowHeight = Dimensions.get('window').height;
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    height: windowHeight,
+  },
+  buttonStyle: {
+    backgroundColor: '#307ecc',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#307ecc',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 15,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
 
-export default HomeScreen;
+export default BluetoothScreen;
